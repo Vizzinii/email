22b729fd6539aac5d2479828a1cd39266c0c1679
京颖 杜 <1095168498@qq.com>
2024-05-28 17:10:39 +0800

    EMail's first commit

---
+++ src/main/java/com/example/email/EMailApplication.java
@@ -0,0 +1,13 @@
+package com.example.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EMailApplication.class, args);
+	}
+
+}
